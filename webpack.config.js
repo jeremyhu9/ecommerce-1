1d71ef9d3a58be2d07b65b245faaf03a5f1e7659
BrianChu <BrianChu13321@gmail.com>
2016-09-10 18:41:18 -0700

    ClientApp.jsx file along with react-router dependency added, refactored interaction with Landing.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/app.js',
+  entry: './js/ClientApp.jsx',
   output: {
     path: path.join(__dirname, '/public'),
     filename: 'bundle.js'
