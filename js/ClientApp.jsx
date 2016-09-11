9ae7fedc95b44e18bef2c12ca52e5e7406242031
jeremyhu9 <jeremyhu415@gmail.com>
2016-09-11 16:56:45 -0700

    Implemented browse link and browseall page

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,6 +1,7 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 const Landing = require('./Landing');
+const Browse = require('./Browseall');
 const { Router, Route, hashHistory } = require('react-router');
 
 const App = React.createClass({
@@ -8,6 +9,7 @@ const App = React.createClass({
     return(
       <Router history = {hashHistory}>
         <Route path='/' component={Landing} />
+        <Route path='browseall' component={Browse} />
       </Router>
     )
   }
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,10 +1,12 @@
 const React = require('react');
+const { Link } = require('react-router');
 
 const Landing = React.createClass({
   render() {
     return (
       <div>
         <h1 className='title'>Computers Emporium</h1>
+        <Link to='browseall' >Browse All </Link>
       </div>
     )
   }
