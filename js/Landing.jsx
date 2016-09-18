780299de644478c63c10eeb1069a28f84479cdcf
jeremyhu9 <jeremyhu415@gmail.com>
2016-09-18 16:19:23 -0700

    Added app.js file and react-bootstrap.

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,20 +1,28 @@
 const React = require('react');
 const { Link } = require('react-router');
-const Featured = require('./Featured')
+const Featured = require('./Featured');
+import { Nav, NavItem } from 'react-bootstrap';
 
 const Landing = React.createClass({
   render() {
     return (
       <div>
         <h1 className='title'>Computers Emporium</h1>
-        <form>
+        <Nav bsStyle="pills" activeKey={1}>
+            <NavItem eventKey={1} href="/landing">Home Page</NavItem>
+            <NavItem eventKey={2} title="/browseall">Browse All</NavItem>
+            <NavItem eventKey={3} disabled>NavItem 3 content</NavItem>
+        </Nav>
+        <form >
           <input type='text' placeholder='Search' />
         </form>
-        <Link to='browseall' >Browse All </Link>
+        // <Link to='browseall' >Browse All </Link>
         <Featured/>
       </div>
     )
   }
-})
+});
+
+
 
 module.exports = Landing;
