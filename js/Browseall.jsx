fdef46b7fd4ecdd298fd09e3bf3fe7b59f257668
BrianChu <BrianChu13321@gmail.com>
2016-10-29 17:41:52 -0700

    created Thumb.jsx, added component to browseall and featured components

--- js/Browseall.jsx
+++ js/Browseall.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const ReactRedux = require('react-redux');
+const Thumb = require('./Thumb');
 const { Link } = require('react-router');
 
 const Browse = React.createClass({
@@ -9,12 +10,7 @@ const Browse = React.createClass({
         <h3>All Items Here</h3>
         <div>
           {this.props.all.map(function(item) {
-            return (
-              <div className='items' key={item.id}>
-                <p>{item.productName}</p>
-                <img src="http://placehold.it/140x100" />
-              </div>
-            )
+            return (<Thumb {...item} key={item.id}/>)
           })}
         </div>
       </div>
